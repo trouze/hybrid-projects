f6d0a08b5afd609f7c3f4a814596ab13a288553b
trouze <tyler@tylerrouze.com>
2025-05-21 13:32:41 -0500

    updates

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,20 +1,3 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-
-    {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
-
-        {{ default_schema }}
-
-    {%- else -%}
-        {%- if env_var('DBT_CLOUD_RUN_REASON_CATEGORY','empty') == 'github_pull_request' -%}
-
-            {{ default_schema }}_{{ custom_schema_name | trim }}
-
-        {%- else -%}
-
-            {{ custom_schema_name | trim }}
-
-        {%- endif -%}
-    {%- endif -%}
-
+    {{ generate_schema_name_for_env(custom_schema_name, node) }}
 {%- endmacro %}
